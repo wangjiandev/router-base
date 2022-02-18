9d8e923005e25cf4c8b7ffe8ec910526d2c80cc7
wangjian <wangjian0504@gmail.com>
2022-02-18 14:44:42 +0800

    第一个版本

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import RouterView from './components/RouterView.vue'
+</script>
+
+<template>
+  <ul>
+    <li><router-link to="/">Home</router-link></li>
+    <li><router-link to="/about">About</router-link></li>
+  </ul>
+  <router-view></router-view>
+</template>
+
+<style></style>
---
+++ src/components/RouterView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { computed } from 'vue'
+import router from '../router'
+import { path } from '../router'
+
+const view = computed(() => {
+  const currentRoute = router.routes.find((route) => route.path === path.value)
+  return currentRoute.component
+})
+</script>
+
+<template>
+  <component :is="view"></component>
+</template>
+
+<style scoped></style>
